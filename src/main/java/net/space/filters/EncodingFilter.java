1c00f0af1e4d477607f40701969aabc991f3d3d8
Albert Akimov <albert.akimov.murom@gmail.com>
2017-08-14 22:22:17 +0300

    fixed

--- src/main/java/net/space/filters/EncodingFilter.java
+++ src/main/java/net/space/filters/EncodingFilter.java
@@ -13,24 +13,28 @@ import java.io.IOException;
  */
 
 public class EncodingFilter implements Filter {
+    private String encoding;
 
-    private String encoding = "utf-8";
-
-    public void doFilter(ServletRequest request,
-
-                         ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        request.setCharacterEncoding(encoding);
-        filterChain.doFilter(request, response);
+    public void init(FilterConfig config) throws ServletException {
+        encoding = config.getInitParameter("requestEncoding");
+        if (encoding == null) encoding = "UTF-8";
     }
 
-    public void init(FilterConfig filterConfig) throws ServletException {
-        String encodingParam = filterConfig.getInitParameter("encoding");
-        if (encodingParam != null) {
-            encoding = encodingParam;
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain next)
+            throws IOException, ServletException {
+        // Respect the client-specified character encoding
+        // (see HTTP specification section 3.4.1)
+        if (null == request.getCharacterEncoding()) {
+            request.setCharacterEncoding(encoding);
         }
+
+        // Set the default response content type and encoding
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        next.doFilter(request, response);
     }
 
     public void destroy() {
-        // nothing todo
     }
 }
