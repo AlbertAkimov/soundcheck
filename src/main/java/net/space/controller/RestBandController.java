8b12814d6ad48e81674df7669318b3af5d28d64f
Albert Akimov <albert.akimov.murom@gmail.com>
2017-08-24 00:45:09 +0300

    fix

--- src/main/java/net/space/controller/RestBandController.java
+++ src/main/java/net/space/controller/RestBandController.java
@@ -1,9 +1,16 @@
 package net.space.controller;
 
+import net.space.model.Band;
 import net.space.service.BandService;
+import net.space.utilities.constants.BandBreakADate;
 import net.space.validators.BandValidator;
+import net.space.validators.json.JsonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,6 +35,29 @@ public class RestBandController {
         this.service = service;
     }
 
+    @PostMapping("/main/add/band")
+    public ResponseEntity<?> addBand(@RequestBody Band band, Errors errors) {
 
+        bandValidator.validate(band, errors);
 
+        JsonResponse jr = new JsonResponse();
+
+        if(errors.hasErrors()) {
+            jr.setStatus("SUCCESS");
+            jr.setResult(errors.getAllErrors());
+
+            return ResponseEntity.badRequest().body(jr);
+        }
+
+        if(band.getId() == 0)
+            service.addBand(BandBreakADate.breakADate(band));
+
+        else
+            service.updateBand(band);
+
+        jr.setStatus("FAIL");
+        jr.setResult(band);
+
+        return ResponseEntity.ok(jr);
+    }
 }
